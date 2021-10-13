92ca4612462978d6eab9216003b37bd6742ffb85
Michi <info@michivonah.ch>
2021-10-13 20:48:26 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const workerCache = "news-michivonah-ch-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/icons/logo-dark-1080.png",
+  "/icons/logo-dark-720.png",
+  "/icons/logo-dark-512.png",
+  "/icons/logo-dark-256.png",
+  "/icons/logo-dark-128.png",
+  "/icons/logo-dark-92.png",
+  "/icons/logo-green-1080.png",
+  "/icons/logo-green-720.png",
+  "/icons/logo-green-512.png",
+  "/icons/logo-green-256.png",
+  "/icons/logo-green-128.png",
+  "/icons/logo-gree -92.png",
+  "/icons/logo-dark-transparent-1080.png",
+  "/icons/logo-green-transparent-1080.png",
+  "https://unpkg.com/aos@2.3.1/dist/aos.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+  "https://unpkg.com/akar-icons-fonts@1.0.8/src/index.js",
+  "https://unpkg.com/akar-icons-fonts/src/css/akar-icons.css"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(workerCache).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
